2b8ee65a5d232ec4e4b98055b97a82a3151f8d77
Miaohuiping <57703957+Miaohuiping@users.noreply.github.com>
2019-12-10 18:36:58 +0800

    发现页面组件和图片

--- gohome/src/store/index.ts
+++ gohome/src/store/index.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { Lazyload } from "vant";
 
+Vue.use(Lazyload);
 Vue.use(Vuex);
 
 export default new Vuex.Store({
---
+++ gohome/src/views/mhp/Find.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="find">
+    <!-- 1.标题 -->
+    <div class="title">
+      <van-nav-bar title="发现" />
+    </div>
+    <!-- 2.轮播 -->
+    <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img v-lazy="image" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      images: [
+        "http://localhost:8080/images/find/swipe/1.jpg",
+        "../../assets/images/find/swipe/2.jpg",
+        "../../assets/images/find/swipe/3.jpg"
+      ]
+    };
+  }
+});
+</script>
+<style lang="scss" scoped>
+.find {
+  background-color: #fff;
+}
+.title {
+  font-weight: bold;
+}
+</style>
