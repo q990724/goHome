48c43945226c15329c39e36ec46f192b4523a7d9
xiejunfeng-ai <58729783+xiejunfeng-ai@users.noreply.github.com>
2019-12-12 00:20:13 +0800

    继续修改

--- gohome/src/views/ak/Images.vue
+++ gohome/src/views/ak/Images.vue
@@ -28,23 +28,69 @@
       <van-tabs @click="onClick">
         <van-tab title="详情">
           <div>
+            <!-- 房间介绍 -->
             <div>
-              <p>上海·整间LOFT</p>
+              <div>
+                <p>上海·整间LOFT</p>
+              </div>
+              <div>
+                <p>
+                  【歐陽·免服务费—新天地精致loft】新天地地铁口/虹桥机场直达/毗邻田子坊思南公馆/步行可达外滩
+                </p>
+              </div>
             </div>
-            <div>
-              <p>
-                【歐陽·免服务费—新天地精致loft】新天地地铁口/虹桥机场直达/毗邻田子坊思南公馆/步行可达外滩
-              </p>
-            </div>
+            <van-divider />
+            <!-- 优惠活动 -->
             <div>
               <div>
                 <p>享受 12月6日 - 12月19日 期间的 9 折优惠</p>
                 <p>在 8 天内完成预订，锁定限时好价</p>
               </div>
               <div>
-                <img src="../../assets/images/ak/naozhong.png" >
+                <img style="width:35px" src="../../assets/images/ak/naozhong.png" >
               </div>
             </div>
+            <van-divider />
+            <!-- 卧室 -->
+            <div>
+              <div>
+                <div>
+                  <img style="width:32px" src="../../assets/images/ak/home.png" alt=""><span>1间卧室</span>
+                  <img style="width:32px" src="../../assets/images/ak/wc.png" alt=""><span>1间卫生间</span>
+                </div>
+                <div>
+                  <img style="width:32px" src="../../assets/images/ak/bed.png" alt=""><span>2张床</span>
+                  <img style="width:32px" src="../../assets/images/ak/people.png" alt=""><span>最多3个人</span>
+                </div>
+              </div>
+            </div>
+            <van-divider />
+            <!-- 房东信息 -->
+            <div>
+              <div>
+                <img src="" alt="">
+                <p>房东：曹昌会</p>
+                <p>共收到228条评价 : 已验证</p>
+              </div>
+              <div>
+                <img src="" alt="">
+              </div>
+            </div>
+            <!-- 房子介绍 -->
+            <div>
+              <p>此房位于市中心外滩南京东路豫园的中心位置，是老式小区，交通便利，小区门对面就是上海最大的书城，购物方便，买东西吃也很方便。到南京东路外滩豫园均可步行到达。</p>
+              <button>更多房源介绍</button>
+            </div>
+            <van-divider />
+            <!-- 卧室小框 -->
+            <div>
+              <div>
+                <p>卧室1</p>
+                <img src="" alt="">
+                <p>1张 1.8 米宽双人床, 1张沙发床</p>
+              </div>
+            </div>
+            <van-divider />
           </div>
         </van-tab>
         <van-tab title="评价">内容 2</van-tab>
