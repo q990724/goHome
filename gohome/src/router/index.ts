75e7a7a7c2174e81eca9ace41a097ae2481e20bc
Jade8998 <57703858+Jade8998@users.noreply.github.com>
2019-12-16 21:26:51 +0800

    21:26修改

--- gohome/src/router/index.ts
+++ gohome/src/router/index.ts
@@ -3,11 +3,13 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../views/cfy/Login.vue";
 import Register from "../views/cfy/Register.vue";
+import Index from "../components/wtq/Index.vue";
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/login", component: Login },
   { path: "/register", component: Register },
+  { path: "/index", component: Index },
 ];
 
 const router = new VueRouter({
